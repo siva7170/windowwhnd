ade49334683a39a9574e99405d40c4da2800edf2
siva7170 <siva7170430@gmail.com>
2020-10-29 10:12:56 +0530

    Small update

--- Whnd/Form1.cs
+++ Whnd/Form1.cs
@@ -122,13 +122,13 @@ namespace Whnd
 
         void gkh_KeyUp(object sender, KeyEventArgs e)
         {
-            lstLog.Items.Add("Up\t" + e.KeyCode.ToString());
+            //lstLog.Items.Add("Up\t" + e.KeyCode.ToString());
             e.Handled = true;
         }
 
         void gkh_KeyDown(object sender, KeyEventArgs e)
         {
-            lstLog.Items.Add("Down\t" + e.KeyCode.ToString());
+            //lstLog.Items.Add("Down\t" + e.KeyCode.ToString());
             e.Handled = true;
         }
     }
